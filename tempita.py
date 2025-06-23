92ff8f53c4f304744fc85db69ff0ecabf4c74930
LiuGaoyong <liugaoyong_88@163.com>
2025-06-23 22:04:53 +0800

    refresh files

--- tempita.py
+++ tempita.py
@@ -1,10 +1,7 @@
-# SPDX-FileCopyrightText: Daniele Nicolodi <daniele@grinta.net>
-# SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
-
 import sys
 from pathlib import Path
 from Cython.Tempita import sub
 
-template = Path(sys.argv[1]).read_text('utf8')
+template = Path(sys.argv[1]).read_text("utf8")
 output = sub(template)
-Path(sys.argv[2]).write_text(output, 'utf8')
+Path(sys.argv[2]).write_text(output, "utf8")
